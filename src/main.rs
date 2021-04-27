4cf9202c0ccffc8d38f3477057b5eb910beef43b
Syudagye <44324659+Syudagye@users.noreply.github.com>
2021-04-27 20:55:02 +0200

    New command line option parsing with clap

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,28 @@
 use std::env;
 use gdsm::{GameSave};
 use std::process::exit;
+use clap::{Clap, AppSettings};
 
 enum Function{
     Help,
     List
 }
 
+#[derive(Clap)]
+#[clap(version = "0.1")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts
+{
+    save_dir: String
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
 
-    let mut game_dir: Option<&str> = None;
+    let opts: Opts = Opts::parse();
 
-    let mut function = Function::Help;
+    let game = GameSave::new(&opts.save_dir);
 
-    for i in 0..args.len() {
-        match args[i].as_str() {
-            "-d" => game_dir = Some(args[i + 1].as_str()),
-            "-h" | "--help" => function = Function::Help,
-            "-l" | "--list" => function = Function::List,
-            _ => ()
-        }
-    }
+    let levels = game.get_local_levels();
+    levels.into_iter().for_each(|l| println!("{}", l.name));
 
-    if game_dir == None {
-        panic!("No game directory specified, use -d for now")
-    }
-
-    let game = GameSave::new(game_dir.unwrap_or(""));
-
-    match function {
-        Function::List => {
-            let levels = game.get_local_levels();
-            levels.into_iter().for_each(|l| println!("{}", l.name))
-        },
-        Function::Help => {
-            println!("Future help menu");
-        }
-    }
 }
